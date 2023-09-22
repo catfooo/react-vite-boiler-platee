c2d28fe9fe2829879637b8236397c402ab7ac709
So Youn Choi <manghatuhyo@gmail.com>
2023-09-22 21:38:04 +0100

    SyntaxError: Importing binding name App is not found.

---
+++ src/Signin.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Signin = () => {
+  return (
+    <div>
+      <div>
+        <button type="button">Sign Out</button>
+        <p>Welcome back, good to see you in here</p>
+      </div>
+      <div>
+        <button type="button">Sign In</button>
+        <p>Please Sign in</p>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
